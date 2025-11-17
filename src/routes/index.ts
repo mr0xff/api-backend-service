1b3310821efb5b7d26892cde2975ac6984897ab8
0xff <worktrabalho633@gmail.com>
2025-11-17 18:34:41 +0100

    cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,17 @@
 import type { FastifyInstance } from "fastify";
 
 export default function index(fastify:FastifyInstance){
-  fastify.all("/", function(req, res){
+  fastify.all("/event_timer", function(_, res){
+    const timing = (new Date).toISOString().split("T")[1].split(/[:\.]/gi).splice(0, 3) as [ string, string, string ];
+    
+    fastify.log.info(timing.toString(), "test");
 
-    res.send(JSON.stringify({ message: "hello"}))
-  });
-
-
-  fastify.all("/sql", function(req, res){
-    const db = new fastify.sqlite.DatabaseSync(":memory:");
-    res.send();
+    res.send({
+      date: new Date(),
+      int: Date.now(),
+      hour: timing[0],
+      minute: timing[1],
+      second: timing[2]
+    })
   })
 }

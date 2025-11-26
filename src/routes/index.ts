580a61d763bcb2f26899e50060a155f562c0a5c7
0xff <worktrabalho633@gmail.com>
2025-11-26 20:02:31 +0100

    setuped

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,30 +1,5 @@
 import { FastifyInstance } from "fastify";
 
 export default function root(fastify: FastifyInstance){
-  fastify.addHook("preHandler", (_, __, done)=>{
-    const schemas = fastify.getSchemas();
-
-    console.log(schemas);
-
-    done();
-  });
-
-  fastify.patch("/", { schema: { body: { $ref: "commonSchema"}}}, (req, res)=>{
-    const body = req.body;
-    fastify.log.warn(body);
-
-    res.send();
-  });
-
-  fastify.post("/", {
-    schema: {
-      body: {
-        $ref: "commonSchema"
-      }
-    }
-  }, (req, res)=>{
-    fastify.log.warn(req.body);
-
-    res.send(fastify.getSchemas());
-  })
+  
 }

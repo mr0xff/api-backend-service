6a6a09e90e5bc79c49fb4c7afb0340979c03ca6e
0xff <worktrabalho633@gmail.com>
2025-12-26 13:29:23 +0100

    add: best pratices

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,19 @@
 import type { FastifyInstance } from "fastify";
 import { readFile } from "fs/promises";
 
+type CID = {
+  code: string;
+  value: string;
+}
 export default async function index(fastify: FastifyInstance){
   fastify.log.warn("inicialização da rota ... ");
 
   const buf = await readFile("cid10.json");
 
-  const ascii = JSON.parse(buf.toString()) as Array<{ code: string; value: string }>;
+  const ascii = (JSON.parse(buf.toString()) as CID[]).map(e =>({
+    valueUpper: e.value.toUpperCase(),
+    ...e
+  }));
 
   const codes = new Map();
 
@@ -15,22 +22,22 @@ export default async function index(fastify: FastifyInstance){
   fastify.get<{Params: { ref: string }}>("/:ref", (req, res)=>{
 
     const { ref } = req.params;
+    const refUpper = ref.toUpperCase();
 
-    if(codes.has(ref.toUpperCase())){
-      res.send(codes.get(ref.toUpperCase()));
+    const r = [];
+
+    if(codes.has(refUpper.toUpperCase())){
+      res.send(codes.get(refUpper.toUpperCase()));
       return;
     }
 
-    const r = ascii.filter (e => {
-      return (new RegExp(ref, "ig"))
-        .test(e.value)
-    });
-    
-    if(r.length > 20)
-      throw fastify.httpErrors.badRequest("A referencia de busca é curta demais!");
-
-    if(!r.length)
-      throw fastify.httpErrors.notFound("Referencia não encontrada!");
+    for(const i of ascii){
+      if(r.length > 20)
+        throw fastify.httpErrors.badRequest("lista longa de resultado, detalhe a referencia!")
+      
+      if(i.valueUpper.includes(refUpper))
+        r.push(i);
+    }
 
     res.send(r);
   });

9dad0ef1d1b199c2ce2ad66f8d47290ecd105e5e
0xff <worktrabalho633@gmail.com>
2026-01-26 13:28:10 +0100

    add: reset

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-export default function index(fastify: FastifyInstance){
-  fastify.get("/", (req, res) => {
-    res.send({ message: "hello world"})
-  })
-}
--- test/routes/example.test.ts
+++ test/routes/example.test.ts
@@ -2,15 +2,12 @@ import { test } from 'node:test'
 import * as assert from 'node:assert'
 import { build } from '../helper.js'
 
-test('mock route', async (t) => {
+test('example is loaded', async (t) => {
   const app = await build(t)
 
   const res = await app.inject({
-    url: '/v1/mock',
-    method: "POST",
-    body: {
-      qty: 2
-    }
+    url: '/example'
   })
-  assert.deepStrictEqual(JSON.parse(res.payload), [ 0, 1 ]);
-});
+
+  assert.equal(res.payload, 'this is an example')
+})

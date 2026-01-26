6c1bc3fbd2ac1b271282de6950f35dc971d8bc28
0xff <worktrabalho633@gmail.com>
2026-01-26 12:33:14 +0100

    add: baseline

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,44 +1,7 @@
-import type { FastifyInstance } from "fastify";
-import { readFile } from "fs/promises";
+import { FastifyInstance } from "fastify";
 
-type CID = {
-  code: string;
-  value: string;
-}
-export default async function index(fastify: FastifyInstance){
-  fastify.log.warn("inicialização da rota ... ");
-
-  const buf = await readFile("cid10.json");
-
-  const ascii = (JSON.parse(buf.toString()) as CID[]).map(e =>({
-    valueUpper: e.value.toUpperCase(),
-    ...e
-  }));
-
-  const codes = new Map();
-
-  ascii.forEach(e => codes.set(e.code, e));
-  
-  fastify.get<{Params: { ref: string }}>("/:ref", (req, res)=>{
-
-    const { ref } = req.params;
-    const refUpper = ref.toUpperCase();
-
-    const r = [];
-
-    if(codes.has(refUpper.toUpperCase())){
-      res.send(codes.get(refUpper.toUpperCase()));
-      return;
-    }
-
-    for(const i of ascii){
-      if(r.length > 20)
-        throw fastify.httpErrors.badRequest("lista longa de resultado, detalhe a referencia!")
-      
-      if(i.valueUpper.includes(refUpper))
-        r.push(i);
-    }
-
-    res.send(r);
-  });
+export default function index(fastify: FastifyInstance){
+  fastify.get("/", (req, res) => {
+    res.send({ message: "hello world"})
+  })
 }

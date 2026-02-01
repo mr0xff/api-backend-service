8d64e8923ce9a41e9db545e0f2988107c2de7dfe
0xff <worktrabalho633@gmail.com>
2026-02-01 23:30:21 +0100

    add: testing dos vuln in 4.8.0 of fastify

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,19 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyInstance, FastifySchema } from "fastify";
 
-const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
+export default function root(fastify: FastifyInstance) {
+  const schema : FastifySchema = {
+    body: {
+      type: "object",
+      properties: {
+        username: { type: "string",enum: ["root", "samurai"] }
+      }
+    }
+  } 
+
+  fastify.post<{ Body: { username: string }}>("/", { schema }, (req, res) => {
+    const { username } = req.body;
+    fastify.log.warn({ username });
+
+    res.send({ msg: "yes" });
   })
 }
-
-export default root
